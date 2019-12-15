c17ff37cb5da4290e9df125d3ac1dc75d0c354d1
heer-boaz <boazpat_el@hotmail.com>
2019-12-15 02:13:05 +0100

    Besturing beterder!!

--- src/bmsx/input.ts
+++ src/bmsx/input.ts
@@ -99,35 +99,62 @@ export class Input {
         window.addEventListener('keyup', e => { preventDefaultEventAction(e); keyup(e.key); }, false);
         window.addEventListener('blur', blur, false);
 
-        document.getElementById('d-pad-u').addEventListener('touchstart', e => keydown('ArrowUp'), false);
-        document.getElementById('d-pad-u').addEventListener('touchend', e => keyup('ArrowUp'), false);
-        document.getElementById('d-pad-ru').addEventListener('touchstart', e => { keydown('ArrowUp'); keydown('ArrowRight'); }, false);
-        document.getElementById('d-pad-ru').addEventListener('touchend', e => { keyup('ArrowUp'); keyup('ArrowRight'); }, false);
-        document.getElementById('d-pad-r').addEventListener('touchstart', e => keydown('ArrowRight'), false);
-        document.getElementById('d-pad-r').addEventListener('touchend', e => keyup('ArrowRight'), false);
-        document.getElementById('d-pad-d').addEventListener('touchstart', e => keydown('ArrowDown'), false);
-        document.getElementById('d-pad-d').addEventListener('touchend', e => keyup('ArrowDown'), false);
-        document.getElementById('d-pad-ld').addEventListener('touchstart', e => { keydown('ArrowLeft'); keydown('ArrowDown'); }, false);
-        document.getElementById('d-pad-ld').addEventListener('touchend', e => { keyup('ArrowLeft'); keyup('ArrowDown'); }, false);
-        document.getElementById('d-pad-l').addEventListener('touchstart', e => keydown('ArrowLeft'), false);
-        document.getElementById('d-pad-l').addEventListener('touchend', e => keyup('ArrowLeft'), false);
-        document.getElementById('d-pad-lu').addEventListener('touchstart', e => { keydown('ArrowLeft'); keydown('ArrowUp'); }, false);
-        document.getElementById('d-pad-lu').addEventListener('touchend', e => { keyup('ArrowLeft'); keyup('ArrowUp'); }, false);
-        document.getElementById('btn1_knop').addEventListener('touchstart', e => keydown(' '), false);
-        document.getElementById('btn1_knop').addEventListener('touchend', e => keyup(' '), false);
-        document.getElementById('btn2_knop').addEventListener('touchstart', e => keydown('m'), false);
-        document.getElementById('btn2_knop').addEventListener('touchend', e => keyup('m'), false);
+        document.getElementById('d-pad-u').addEventListener('touchstart', e => keydown('ArrowUp'));
+        // document.getElementById('d-pad-u').addEventListener('click', e => keydown('ArrowUp'), false);
+        document.getElementById('d-pad-u').addEventListener('touchend', e => keyup('ArrowUp'));
+        document.getElementById('d-pad-ru').addEventListener('touchstart', e => { keydown('ArrowUp'); keydown('ArrowRight'); });
+        // document.getElementById('d-pad-ru').addEventListener('click', e => { keydown('ArrowUp'); keydown('ArrowRight'); }, false);
+        document.getElementById('d-pad-ru').addEventListener('touchend', e => { keyup('ArrowUp'); keyup('ArrowRight'); });
+        document.getElementById('d-pad-r').addEventListener('touchstart', e => keydown('ArrowRight'));
+        // document.getElementById('d-pad-r').addEventListener('click', e => keydown('ArrowRight'), false);
+        document.getElementById('d-pad-r').addEventListener('touchend', e => keyup('ArrowRight'));
+        document.getElementById('d-pad-rd').addEventListener('touchstart', e => { keydown('ArrowDown'); keydown('ArrowRight'); });
+        // document.getElementById('d-pad-rd').addEventListener('click', e => { keydown('ArrowDown'); keydown('ArrowRight'); }, false);
+        document.getElementById('d-pad-rd').addEventListener('touchend', e => { keyup('ArrowDown'); keyup('ArrowRight'); });
+        document.getElementById('d-pad-d').addEventListener('touchstart', e => keydown('ArrowDown'));
+        // document.getElementById('d-pad-d').addEventListener('click', e => keydown('ArrowDown'), false);
+        document.getElementById('d-pad-d').addEventListener('touchend', e => keyup('ArrowDown'));
+        document.getElementById('d-pad-ld').addEventListener('touchstart', e => { keydown('ArrowLeft'); keydown('ArrowDown'); });
+        // document.getElementById('d-pad-ld').addEventListener('click', e => { keydown('ArrowLeft'); keydown('ArrowDown'); }, false);
+        document.getElementById('d-pad-ld').addEventListener('touchend', e => { keyup('ArrowLeft'); keyup('ArrowDown'); });
+        document.getElementById('d-pad-l').addEventListener('touchstart', e => keydown('ArrowLeft'));
+        // document.getElementById('d-pad-l').addEventListener('click', e => keydown('ArrowLeft'), false);
+        document.getElementById('d-pad-l').addEventListener('touchend', e => keyup('ArrowLeft'));
+        document.getElementById('d-pad-lu').addEventListener('touchstart', e => { keydown('ArrowLeft'); keydown('ArrowUp'); });
+        // document.getElementById('d-pad-lu').addEventListener('click', e => { keydown('ArrowLeft'); keydown('ArrowUp'); }, false);
+        document.getElementById('d-pad-lu').addEventListener('touchend', e => { keyup('ArrowLeft'); keyup('ArrowUp'); });
+        document.getElementById('btn1_knop').addEventListener('touchstart', e => keydown(' '));
+        document.getElementById('btn1_knop').addEventListener('click', e => keydown(' '));
+        document.getElementById('btn1_knop').addEventListener('touchend', e => keyup(' '));
+        document.getElementById('btn2_knop').addEventListener('touchstart', e => keydown('m'));
+        document.getElementById('btn2_knop').addEventListener('click', e => keydown('m'));
+        document.getElementById('btn2_knop').addEventListener('touchend', e => keyup('m'));
+        document.addEventListener('touchmove', e => detectElementUnderMove(e), false);
+        document.addEventListener('touchend', e => Input.reset(), false);
+        document.addEventListener('touchcancel', e => Input.reset(), false);
     }
 
-    public static reset(): void {
+    public static reset(except?: string[]): void {
         let props = Object.keys(Input.KeyState);
         for (let i = 0; i < props.length; i++) {
-            delete Input.KeyState[props[i]];
+            if (!except || except.indexOf(props[i]) === -1) { delete Input.KeyState[props[i]]; }
         }
+
         props = Object.keys(Input.KeyClickRequestedState);
         for (let i = 0; i < props.length; i++) {
-            delete Input.KeyClickRequestedState[props[i]];
+            if (!except || except.indexOf(props[i]) === -1) { delete Input.KeyClickRequestedState[props[i]]; }
         }
+
+        document.getElementById('d-pad-u').classList.remove('druk');
+        document.getElementById('d-pad-ru').classList.remove('druk');
+        document.getElementById('d-pad-r').classList.remove('druk');
+        document.getElementById('d-pad-rd').classList.remove('druk');
+        document.getElementById('d-pad-d').classList.remove('druk');
+        document.getElementById('d-pad-ld').classList.remove('druk');
+        document.getElementById('d-pad-l').classList.remove('druk');
+        document.getElementById('d-pad-lu').classList.remove('druk');
+        document.getElementById('btn1_knop').classList.remove('druk');
+        document.getElementById('btn2_knop').classList.remove('druk');
     }
 }
 
@@ -159,6 +186,69 @@ function keyup(key: string): void {
 
 function blur(e: FocusEvent): void {
     Input.reset();
+}
+
+function detectElementUnderMove(e: TouchEvent): void {
+    let pos = e.changedTouches[0];
+    let elementTouched = document.elementFromPoint(pos.clientX, pos.clientY);
+    if (!elementTouched) return;
+
+    switch (elementTouched.id) {
+        case 'd-pad-u':
+            keydown('ArrowUp');
+            Input.reset([ 'ArrowUp' ]);
+            document.getElementById('d-pad-u').classList.add('druk');
+            break;
+        case 'd-pad-ru':
+            keydown('ArrowUp');
+            keydown('ArrowRight');
+            Input.reset(['ArrowUp', 'ArrowRight']);
+            document.getElementById('d-pad-ru').classList.add('druk');
+        break;
+        case 'd-pad-r':
+            keydown('ArrowRight');
+            Input.reset(['ArrowRight']);
+            document.getElementById('d-pad-r').classList.add('druk');
+        break;
+        case 'd-pad-rd':
+            keydown('ArrowRight');
+            keydown('ArrowDown');
+            Input.reset(['ArrowDown', 'ArrowRight']);
+            document.getElementById('d-pad-rd').classList.add('druk');
+        break;
+        case 'd-pad-d':
+            keydown('ArrowDown');
+            Input.reset(['ArrowDown']);
+            document.getElementById('d-pad-d').classList.add('druk');
+        break;
+        case 'd-pad-ld':
+            keydown('ArrowLeft');
+            keydown('ArrowDown');
+            Input.reset(['ArrowLeft', 'ArrowDown']);
+            document.getElementById('d-pad-ld').classList.add('druk');
+        break;
+        case 'd-pad-l':
+            keydown('ArrowLeft');
+            Input.reset(['ArrowLeft']);
+            document.getElementById('d-pad-l').classList.add('druk');
+        break;
+        case 'd-pad-lu':
+            keydown('ArrowLeft');
+            keydown('ArrowUp');
+            Input.reset(['ArrowUp', 'ArrowLeft']);
+            document.getElementById('d-pad-lu').classList.add('druk');
+        break;
+        case 'btn1_knop':
+            keydown(' ');
+            Input.reset([' ']);
+            document.getElementById('btn1_knop').classList.add('druk');
+        break;
+        case 'btn2_knop':
+            keydown('m');
+            Input.reset(['m']);
+            document.getElementById('btn2_knop').classList.add('druk');
+        break;
+    }
 }
 
 // export function getMousePos(evt: MouseEvent): Point {
