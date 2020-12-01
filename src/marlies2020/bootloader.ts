81c35a3cab1da60efa6e1ce2381f39920745e1dc
heer-boaz <boazpat_el@hotmail.com>
2020-12-01 18:24:44 +0100

    Added new rom voor Sinterklaas 2020 :-)

---
+++ src/marlies2020/bootloader.ts
@@ -0,0 +1,66 @@
+import { RomLoadResult } from '../bmsx/rompack';
+import { Game, game, model, BaseModel, IGameObject, Sprite } from '../bmsx/engine';
+import { setPoint, newPoint, Direction, newSize } from '../bmsx/common';
+import { Tile, MSX1ScreenWidth, MSX1ScreenHeight } from '../bmsx/msx';
+import { GLView } from '../bmsx/glview';
+import { BitmapId } from './resourceids';
+import { Input } from '../bmsx/input';
+
+let bclass = class extends Sprite {
+    constructor() {
+        super();
+        this.imgid = BitmapId.b;
+    }
+
+    takeTurn(): void {
+        if (Input.KD_UP) {
+            this.pos.y -= 2;
+        }
+        if (Input.KD_RIGHT) {
+            this.pos.x += 2;
+        }
+        if (Input.KD_DOWN) {
+            this.pos.y += 2;
+        }
+        if (Input.KD_LEFT) {
+            this.pos.x -= 2;
+        }
+    }
+
+};
+
+let _modelclass = class extends BaseModel {
+    public get gamewidth(): number {
+        return MSX1ScreenWidth;
+    }
+
+    public get gameheight(): number {
+        return MSX1ScreenHeight;
+    }
+
+    public collidesWithTile(o: IGameObject, dir: Direction): boolean {
+        return false;
+    }
+
+    public isCollisionTile(x: number, y: number): boolean {
+        return false;
+    }
+};
+
+
+let _viewclass = class extends GLView {
+    public drawgame() {
+        super.drawgame();
+        super.drawSprites();
+    }
+};
+
+var _global = window || global;
+_global['h406A'] = (rom: RomLoadResult, sndcontext: AudioContext, gainnode: GainNode): void => {
+    let _model = new _modelclass();
+    let _view = new _viewclass(newSize(MSX1ScreenWidth, MSX1ScreenHeight));
+    new Game(rom, _model, _view, null, sndcontext, gainnode);
+
+    game.start();
+    model.spawn(new bclass(), newPoint(100, 100));
+};
---
+++ src/marlies2020/resourceids.ts
@@ -0,0 +1,9 @@
+export enum BitmapId {
+	None = 0,
+	b = 1,
+	_atlas = 2,
+}
+
+export enum AudioId {
+	None = 0,
+}
