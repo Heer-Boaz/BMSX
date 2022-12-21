22b6d736001a38e8e6dcae61a3d25b762d71256d
Boaz <boaz.el.pat@rabobank.nl>
2022-12-21 18:54:59 +0100

    Serialize object gerefactored.

--- src/bmsx/gamereviver.ts
+++ src/bmsx/gamereviver.ts
@@ -6,43 +6,64 @@ interface IReviver {
 	(key: any, value: any): any;
 	constructors: Record<string, new () => any>;
 	onLoad: Record<string, (result: any) => any>;
+	/**
+	 * Remove the helper-property that was added during serialization
+	 */
+	removeSerializerProps: (obj: { typename: string; }) => void;
 }
 
 interface ISerializer {
 	(obj: any): string;
 	excludedProperties: class2propkey2bool;
+	/**
+	 * Returns whether the given value should be excluded from serialization,
+	 * based on @see {@link excludedProperties} and value type (e.g. `function`).
+	 */
+	shouldExcludeFromSerialization: (key: any, value: any, cache: any[]) => boolean;
+	serializeObject: (value: any, cache: any[]) => {};
 }
 
 export const Serializer: ISerializer = (obj: any): string => {
 	let cache = [];
 	return JSON.stringify(obj, function (key, value) {
-		// Verify whether this property should be excluded from serialization
-		let typename = value?.constructor?.name ?? value?.prototype?.name;
-		if (typename && key) {
-			if (Serializer.excludedProperties[typename]?.[key])
-				return undefined; // Exclude property from serialization
-		}
-		if (Array.isArray(value)) return value;
-		let type = typeof value;
-		switch (type) {
-			case 'object':
-				if (cache.includes(value)) return undefined;
-				cache.push(value);
-				if (typename !== 'Object' && typename !== 'object') {
-					value.typename = typename;
-					if (value.prototype?.onsave) return value.prototype.onsave(value);
-					if (value.constructor?.onsave) return value.constructor.onsave(value);
-				}
-				return value;
-			case 'function':
-				return undefined;
-			default:
-				return value;
-		}
+		if (Serializer.shouldExcludeFromSerialization(key, value, cache))
+			return undefined;
+		if (typeof value === 'object')
+			return Serializer.serializeObject(value, cache);
+		return value;
 	});
 }
 
 Serializer.excludedProperties = Serializer.excludedProperties ?? {};
+Serializer.shouldExcludeFromSerialization = (key: any, value: any, cache: any[]): boolean => {
+	let typename = value?.constructor?.name ?? value?.prototype?.name;
+	if (typename && key) {
+		if (Serializer.excludedProperties[typename]?.[key])
+			return true; // Exclude property from serialization
+	}
+	let type = typeof value;
+	switch (type) {
+		case 'function':
+			return true; // We don't serialize functions
+		case 'object':
+			return cache.includes(value);
+		default:
+			return false;
+	}
+}
+
+Serializer.serializeObject = (value: any, cache: any[]): {} => {
+	if (Array.isArray(value)) return value;
+	cache.push(value); // We already checked whether object should be excluded in Serializer.shouldExcludeFromSerialization
+
+	let typename = value?.constructor?.name ?? value?.prototype?.name; // TODO: Twee keer bepalen :-(
+	if (typename !== 'Object' && typename !== 'object') {
+		value.typename = typename;
+		if (value.prototype?.onsave) return value.prototype.onsave(value);
+		if (value.constructor?.onsave) return value.constructor.onsave(value);
+	}
+	return value;
+}
 
 // https://stackoverflow.com/questions/8111446/turning-json-strings-into-objects-with-methods
 // A generic "smart reviver" function.
@@ -63,13 +84,21 @@ export const Reviver: IReviver = (key: any, value: any) => {
 		if (!theConstructor) throw `No constructor known for object of type '${value.typename}'. Did you forget to add '@insavegame' to the class definition?`;
 		let result = Object.assign(new theConstructor(), value);
 		let onload = Reviver.onLoad[value.typename];
-		return onload ? onload(result) : result;
+		onload && (result = onload(result));
+		Reviver.removeSerializerProps(result);
+		return result;
 	}
 	return value;
 }
 
 Reviver.constructors = Reviver.constructors ?? {};
 Reviver.onLoad = Reviver.onLoad ?? {};
+Reviver.removeSerializerProps = (obj: { typename: string }): void => {
+	// Remove the helper-property that was added during serialization
+	obj.typename = undefined;
+	delete obj.typename;
+}
+
 // Reviver.onSave = Reviver.onSave ?? {};
 
 // target: the class that the member is on.
