beb7eea4190e6605098f33f59c6f2f2cbee457b2
heer-boaz <boazpat_el@hotmail.com>
2023-04-19 03:01:11 +0200

    GRAFENTEKENEN!

--- scripts/graph_outputter.js
+++ scripts/graph_outputter.js
@@ -1,16 +1,39 @@
-const dagre = require("dagre");
+// const dagre = require('dagre');
 
 class Vertex {
-    constructor(id, label = "") {
+    constructor(id, label = '') {
         this.id = id;
         this.label = label;
         this.edges = [];
+        this.pos = { x: 0, y: 0 };
     }
 
-    connectTo(vertex, label = "") {
+    connectTo(vertex, label = '') {
         const edge = new Edge(this, vertex, label);
-        this.edges.push(edge);
+        this.edges.push(edge)
+            ;
         return edge;
+    }
+
+    print() {
+        logObjectProperties(this);
+    }
+}
+
+function logObjectProperties(obj, visited = new Set()) {
+    for (let key in obj) {
+        const value = obj[key];
+        if (typeof value === 'object' && value !== null) {
+            if (visited.has(value)) {
+                // console.log(`${key}: [Circular]`);
+            } else {
+                visited.add(value);
+                // console.log(`${key}...`);
+                logObjectProperties(value, visited);
+            }
+        } else {
+            console.log(`${key}: ${value}`);
+        }
     }
 }
 
@@ -34,14 +57,17 @@ class Graph {
         return this.vertices.find((vertex) => vertex.id === id);
     }
 
-    toAscii(width = 80, height = 40) {
+    toAscii(width = 20, height = 20) {
         // Determine the maximum label length
         const maxLabelLength = this.vertices.reduce((max, vertex) => {
             return Math.max(max, vertex.label.length);
         }, 0);
 
         // Calculate matrix dimensions
-        const { matrixWidth, matrixHeight } = dagreLayout(graph);
+        // const { matrixWidth, matrixHeight } = dagreLayout(graph, maxLabelLength);
+        // const { matrixWidth, matrixHeight } = flowchartLayout(graph);
+        const { matrixWidth, matrixHeight } = layeredGraphLayout(graph);
+        console.log(`${matrixWidth}, ${matrixHeight}`);
 
         // Draw the vertices and edges on the grid
         const matrix = Array(matrixHeight)
@@ -77,49 +103,173 @@ function equalPos(pos1, pos2) {
     return pos1.x === pos2.x && pos1.y === pos2.y;
 }
 
-function dagreLayout(graph) {
-    const g = new dagre.graphlib.Graph();
-    g.setGraph({});
-    g.setDefaultEdgeLabel(() => ({}));
+function layeredGraphLayout(graph) {
+    // Sort the nodes in topological order
+    const sortedNodes = sortTopologically(graph);
 
-    // Voeg knooppunten toe aan de dagre-grafiek
-    graph.vertices.forEach((vertex) => {
-        g.setNode(vertex.id, { width: 80, height: 30 });
+    // Assign layers to the nodes
+    const layers = [];
+    sortedNodes.forEach((node) => {
+        let layerIndex = 0;
+        while (layers[layerIndex] && layers[layerIndex].some((otherNode) => otherNode.edges.some((edge) => edge.vertices.includes(node)))) {
+            layerIndex += 1;
+        }
+        node.layer = layerIndex;
+        if (!layers[layerIndex]) {
+            layers[layerIndex] = [];
+        }
+        layers[layerIndex].push(node);
     });
 
-    // Voeg randen toe aan de dagre-grafiek
-    graph.vertices.forEach((vertex) => {
-        vertex.edges.forEach((edge) => {
-            const otherVertex = edge.vertices.find((v) => v !== vertex);
-            g.setEdge(vertex.id, otherVertex.id);
+    // Position the nodes within each layer
+    layers.forEach((layer, layerIndex) => {
+        const layerWidth = Math.max(...layer.map(getNodeWidth));
+        const layerHeight = layer.reduce((sum, node) => sum + getNodeHeight(node), 0);
+        let x = layerWidth / 2 + layerIndex * layerWidth;
+        let y = 0;
+        layer.forEach((node, nodeIndex) => {
+            const layerLength = layer.length > 1 ? layer.length - 1 : 1;
+            node.pos.x = x;
+            node.pos.y = y + nodeIndex * (layerHeight / layerLength);
+            y += getNodeHeight(node);
+            x += layerWidth;
         });
     });
 
-    // Bereken de lay-out
-    dagre.layout(g);
+    // Adjust y-coordinates to make the graph planar, if possible
+    // Adjust y-coordinates to make the graph planar, if possible
+    for (const vertex of graph.vertices) {
+        const outgoingEdges = vertex.edges.filter(edge => edge.vertices[0] === vertex);
+        if (outgoingEdges.length > 1) {
+            const deltaY = getNodeHeight(vertex);
+            let shiftY = deltaY;
+            outgoingEdges.forEach((edge, index) => {
+                const targetVertex = edge.vertices.find(v => v !== vertex);
+                if (index > 0) {
+                    targetVertex.pos.y += shiftY;
+                    shiftY += deltaY;
+                }
+            });
+        }
+    }
 
-    // Wijs de berekende posities toe aan de oorspronkelijke grafiek
-    g.nodes().forEach((nodeId) => {
-        const node = g.node(nodeId);
-        const vertex = graph.getVertexById(nodeId);
-        vertex.pos = { x: node.x, y: node.y };
-    });
+    // Determine the width and height of the graph
+    const maxX = Math.max(...graph.vertices.map((vertex) => vertex.pos.x + getNodeWidth(vertex)));
+    const maxY = Math.max(...graph.vertices.map((vertex) => vertex.pos.y + getNodeHeight(vertex)));
+    const matrixWidth = Math.ceil(maxX) + 0;
+    const matrixHeight = Math.ceil(maxY) + 0;
 
-    // Calculate matrix dimensions based on dagreLayout output
-    const matrixWidth = Math.ceil(g.graph().width) + 1;
-    const matrixHeight = Math.ceil(g.graph().height) + 1;
+    // Return the width and height of the graph
     return { matrixWidth, matrixHeight };
 }
 
+function flowchartLayout(graph) {
+    // Sort the nodes in topological order
+    const sortedNodes = sortTopologically(graph);
 
-function drawMatrixPixel(matrix,x, y,  char) {
+    // Assign layers to the nodes
+    const layers = [];
+    let layerIndex = 0;
+    sortedNodes.forEach((node) => {
+        while (layers[layerIndex] && layers[layerIndex].some((otherNode) => otherNode.edges.some((edge) => edge.vertices.includes(node)))) {
+            layerIndex += 1;
+        }
+        node.layer = layerIndex;
+        if (!layers[layerIndex]) {
+            layers[layerIndex] = [];
+        }
+        layers[layerIndex].push(node);
+    });
+
+
+    // Position the nodes within each layer
+    layers.forEach((layer, layerIndex) => {
+        const layerWidth = Math.max(...layer.map(getNodeWidth));
+        const layerHeight = layer.reduce((sum, node) => sum + getNodeHeight(node), 0);
+        let x = layerWidth / 2 + layerIndex * layerWidth;
+        let y = 0;
+        layer.forEach((node, nodeIndex) => {
+            const layerLength = layer.length > 1 ? layer.length - 1 : 1;
+            node.pos.x = x;
+            node.pos.y = y + nodeIndex * (layerHeight / layerLength);
+            y += getNodeHeight(node);
+            x += layerWidth;
+        });
+    });
+
+    // Adjust y-coordinates to make the graph planar, if possible
+    const visitedVertices = new Set();
+    const dfs = (vertex, depth = 0) => {
+        visitedVertices.add(vertex);
+        vertex.pos.y = depth;
+
+        const outgoingEdges = vertex.edges.filter(
+            (edge) => edge.vertices[0] === vertex
+        );
+        const targetVertices = outgoingEdges.map((edge) => edge.vertices[1]);
+        targetVertices.forEach((targetVertex) => {
+            if (!visitedVertices.has(targetVertex)) {
+                dfs(targetVertex, depth + 1);
+            }
+        });
+    };
+    graph.vertices.forEach((vertex) => {
+        if (!visitedVertices.has(vertex)) {
+            dfs(vertex);
+        }
+    });
+
+    // Determine the width and height of the graph
+    const maxX = Math.max(...graph.vertices.map((vertex) => vertex.pos.x + getNodeWidth(vertex)));
+    const maxY = Math.max(...graph.vertices.map((vertex) => vertex.pos.y + getNodeHeight(vertex)));
+    const matrixWidth = Math.ceil(maxX) + 20;
+    const matrixHeight = Math.ceil(maxY) + 20;
+
+    // Return the width and height of the graph
+    return { matrixWidth, matrixHeight };
+}
+
+function sortTopologically(graph) {
+    const sortedNodes = [];
+    const visited = new Set();
+
+    function visit(node) {
+        if (!visited.has(node)) {
+            visited.add(node);
+
+            node.edges.forEach((edge) => {
+                const otherNode = edge.vertices.find((v) => v !== node);
+                visit(otherNode);
+            });
+
+            sortedNodes.push(node);
+        }
+    }
+
+    graph.vertices.forEach((vertex) => visit(vertex));
+
+    return sortedNodes.reverse();
+}
+
+function getNodeWidth(node) {
+    // Return the width of the node
+    return (node?.label?.length + 2) ?? 12;
+}
+
+function getNodeHeight(node) {
+    // Return the height of the node
+    return 5;
+}
+
+function drawMatrixPixel(matrix, x, y, char, overwrite = true) {
     if (y < 0) return;
     if (y >= matrix.length) return;
     if (x < 0) return;
-    if (!matrix[y]) throw `${y}`;
     if (x >= matrix[y].length) return;
 
-    matrix[y][x] = char;
+    if (overwrite || (!matrix[y][x] || matrix[y][x] === " ")) {
+        matrix[y][x] = char;
+    }
 }
 
 function drawVertex(matrix, vertex, vertexPositionOverride, labelLengthOverride) {
@@ -170,17 +320,16 @@ function drawEdge(matrix, v1, v2) {
         }
 
         // Check for diagonal movement and adjust the character
-        if (x1 !== x2 && y1 !== y2) {
-            char = '°';
+        if (x1 !== x2 && y1 !== y2 && Math.abs(x1 - x2) > 2) {
             if ((x1 < x2 && y1 > y2) || (x1 > x2 && y1 < y2)) {
-                char = '°';
+                char = '/';
+            } else {
+                char = '\\';
             }
         }
 
         if (x1 >= 0 && x1 < matrix[0].length && y1 >= 0 && y1 < matrix.length) {
-            if (!matrix[y1][x1] || matrix[y1][x1] === " ") {
-                matrix[y1][x1] = char;
-            }
+            drawMatrixPixel(matrix, x1, y1, char, false);
         }
 
         if (x1 === x2 && y1 === y2) {
@@ -199,21 +348,32 @@ function drawEdge(matrix, v1, v2) {
     }
 }
 
+
 // Example usage
 const graph = new Graph();
 
-const vertex1 = new Vertex("1", "Node 1");
+const vertex1 = new Vertex("1", "Node 1sdfsdfsdf");
 const vertex2 = new Vertex("2", "Node 2");
-const vertex3 = new Vertex("3", "Node 3");
+const vertex3 = new Vertex("3", "Node 3sdfdsfs\adflsdkfjsdfl");
+const vertex4 = new Vertex("4", "Node 4");
+const vertex5 = new Vertex("5", "Node 5");
 
 graph.addVertex(vertex1);
 graph.addVertex(vertex2);
 graph.addVertex(vertex3);
+graph.addVertex(vertex4);
+graph.addVertex(vertex5);
 
 vertex1.connectTo(vertex2);
 vertex1.connectTo(vertex3);
-vertex3.connectTo(vertex1);
-vertex2.connectTo(vertex3);
+vertex1.connectTo(vertex4);
+vertex1.connectTo(vertex5);
+// vertex2.connectTo(vertex3);
+// vertex2.connectTo(vertex4);
+// vertex2.connectTo(vertex5);
+// vertex3.connectTo(vertex4);
+// vertex3.connectTo(vertex5);
+// vertex4.connectTo(vertex5);
 
 console.log(graph.toAscii());
 
@@ -284,4 +444,197 @@ console.log(graph.toAscii());
 //             console.log(vertex.pos.y);
 //         });
 //     }
+// }
+
+// function dagreLayout(graph, maxLabelLength) {
+//     const g = new dagre.graphlib.Graph();
+//     g.setGraph({});
+//     g.setDefaultEdgeLabel(() => ({}));
+
+//     // Voeg knooppunten toe aan de dagre-grafiek
+//     graph.vertices.forEach((vertex) => {
+//         g.setNode(vertex.id, { width: maxLabelLength + 2, height: 3 });
+//     });
+
+//     // Voeg randen toe aan de dagre-grafiek
+//     graph.vertices.forEach((vertex) => {
+//         vertex.edges.forEach((edge) => {
+//             const otherVertex = edge.vertices.find((v) => v !== vertex);
+//             g.setEdge(vertex.id, otherVertex.id);
+//         });
+//     });
+
+//     // Bereken de lay-out
+//     dagre.layout(g);
+
+//     // Wijs de berekende posities toe aan de oorspronkelijke grafiek
+//     g.nodes().forEach((nodeId) => {
+//         const node = g.node(nodeId);
+//         const vertex = graph.getVertexById(nodeId);
+//         vertex.pos = { x: node.x, y: node.y };
+//     });
+
+//     // Calculate matrix dimensions based on dagreLayout output
+//     const matrixWidth = Math.ceil(g.graph().width) + 1;
+//     const matrixHeight = Math.ceil(g.graph().height) + 1;
+//     return { matrixWidth, matrixHeight };
+// }
+
+// function layeredGraphLayout(graph) {
+//     // Sort the nodes in topological order
+//     const sortedNodes = sortTopologically(graph);
+
+//     // Assign layers to the nodes
+//     const layers = [];
+//     sortedNodes.forEach((node) => {
+//         let layerIndex = 0;
+//         while (
+//             layers[layerIndex] &&
+//             layers[layerIndex].some((otherNode) =>
+//                 otherNode.edges.some((edge) => edge.vertices.includes(node))
+//             )
+//         ) {
+//             layerIndex += 1;
+//         }
+//         node.layer = layerIndex;
+//         if (!layers[layerIndex]) {
+//             layers[layerIndex] = [];
+//         }
+//         layers[layerIndex].push(node);
+//     });
+
+//     // Position the nodes within each layer
+//     layers.forEach((layer, layerIndex) => {
+//         const layerWidth = Math.max(...layer.map(getNodeWidth));
+//         const layerHeight = layer.reduce(
+//             (sum, node) => sum + getNodeHeight(node),
+//             0
+//         );
+//         let x = layerWidth / 2 + layerIndex * layerWidth;
+//         let y = 0;
+//         const connectionsByTarget = {};
+//         layer.forEach((node, nodeIndex) => {
+//             const layerLength = layer.length > 1 ? layer.length - 1 : 1;
+//             node.pos.x = x;
+//             node.pos.y = y + nodeIndex * (layerHeight / layerLength);
+//             y += getNodeHeight(node);
+//             x += layerWidth;
+
+//             // Collect all outgoing edges for this node grouped by target
+//             node.edges.forEach((edge) => {
+//                 const target = edge.vertices[1];
+//                 if (!connectionsByTarget[target.id]) {
+//                     connectionsByTarget[target.id] = [];
+//                 }
+//                 connectionsByTarget[target.id].push(edge);
+//             });
+//         });
+
+//         // Adjust y-coordinates to make the graph planar, if possible
+//         Object.entries(connectionsByTarget).forEach(([targetId, outgoingEdges]) => {
+//             if (outgoingEdges.length > 1) {
+//                 const targetNode = graph.vertices.find((v) => v.id === targetId);
+//                 const targetPos = targetNode.pos;
+//                 const midIndex = Math.floor(outgoingEdges.length / 2);
+//                 const midEdge = outgoingEdges[midIndex];
+//                 const sourceNode = midEdge.vertices[0];
+//                 const sourcePos = sourceNode.pos;
+//                 const yDelta = getNodeHeight(targetNode) * 0.5;
+//                 const yOffset = yDelta / (outgoingEdges.length - 1);
+
+//                 // Move the target vertex up or down to create space for the other connections
+//                 let yShift = -yDelta / 2;
+//                 outgoingEdges.forEach((edge, index) => {
+//                     const sourceNode = edge.vertices[0];
+//                     if (!equalPos(sourceNode.pos, sourcePos)) {
+//                         const sourceEdges = sourceNode.edges.filter(
+//                             (e) => e.vertices[1] !== targetNode
+//                         );
+//                         const sourceEdgeIndex = sourceEdges.findIndex(
+//                             (e) => equalPos(e.vertices[1].pos, targetPos)
+//                         );
+//                         const sourceEdgeCount = sourceEdges.length;
+//                         const sourceYDelta = getNodeHeight(sourceNode) * 0.5;
+//                         const sourceYOffset = sourceYDelta / (sourceEdgeCount - 1);
+//                         const sourceYShift =
+//                             -sourceYDelta / 2 + sourceEdgeIndex * sourceYOffset;
+//                         targetNode.pos.y += sourceYShift - yShift;
+//                     }
+//                     yShift += yOffset;
+//                 });
+//             }
+//         });
+//         // Set the y-position of each node within the layer
+//         layer.forEach((node) => {
+//             const layerLength = layer.length > 1 ? layer.length - 1 : 1;
+//             node.pos.y = node.pos.y - layerHeight / layerLength / 2;
+//         });
+//     });
+
+//     // Determine the width and height of the graph
+//     const maxX = Math.max(
+//         ...graph.vertices.map(
+//             (vertex) => vertex.pos.x + getNodeWidth(vertex)
+//         )
+//     );
+//     const maxY = Math.max(
+//         ...graph.vertices.map(
+//             (vertex) => vertex.pos.y + getNodeHeight(vertex)
+//         )
+//     );
+//     const matrixWidth = Math.ceil(maxX) + 20;
+//     const matrixHeight = Math.ceil(maxY) + 20;
+
+//     // Return the width and height of the graph
+//     return { matrixWidth, matrixHeight };
+// }
+
+// function flowchartLayout(graph) {
+//     // Sort the nodes in topological order
+//     const sortedNodes = sortTopologically(graph);
+
+//     // Determine the maximum width and height of each layer
+//     const layerDimensions = [];
+//     sortedNodes.forEach((node) => {
+//         node.layer = 0;
+//         const layerIndex = node.layer;
+//         const nodeWidth = getNodeWidth(node);
+//         const nodeHeight = getNodeHeight(node);
+//         const layerWidth = layerDimensions[layerIndex]?.width ?? 0;
+//         const layerHeight = layerDimensions[layerIndex]?.height ?? 0;
+//         layerDimensions[layerIndex] = {
+//             width: Math.max(layerWidth, nodeWidth),
+//             height: layerHeight + nodeHeight,
+//         };
+//     });
+
+//     // Position the nodes within each layer
+//     layerDimensions.forEach((layerDimension, layerIndex) => {
+//         const nodesInLayer = sortedNodes.filter((node) => node.layer === layerIndex);
+//         const numNodesInLayer = nodesInLayer.length;
+//         const layerWidth = layerDimension.width;
+//         const layerHeight = layerDimension.height;
+//         const yStart = (1 - layerHeight) / 2;
+//         let xStart = (1 - layerWidth) / 2;
+
+//         nodesInLayer.forEach((node, nodeIndex) => {
+//             const nodeWidth = getNodeWidth(node);
+//             const nodeHeight = getNodeHeight(node);
+//             const x = xStart + (nodeWidth / 2);
+//             const y = yStart + (nodeHeight / 2) + (nodeIndex / (numNodesInLayer - 1)) * (layerHeight - nodeHeight);
+//             node.pos.x = x;
+//             node.pos.y = y;
+//         });
+
+//         xStart += layerWidth;
+//     });
+
+//     // Determine the width and height of the graph
+//     const maxX = Math.max(...graph.vertices.map((vertex) => vertex.pos.x + getNodeWidth(vertex)));
+//     const maxY = Math.max(...graph.vertices.map((vertex) => vertex.pos.y + getNodeHeight(vertex)));
+//     const matrixWidth = Math.ceil(maxX) + 20;
+//     const matrixHeight = Math.ceil(maxY) + 20;
+
+//     // Return the width and height of the graph
+//     return { matrixWidth, matrixHeight };
 // }
