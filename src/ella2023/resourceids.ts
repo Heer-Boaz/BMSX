1de30334bd736b8d7d04715dc9673d508f8de5bf
Boaz Pat-El <boazpat_el@hotmail.com>
2023-12-13 18:19:33 +0100

    Add new bitmap resources

--- src/ella2023/resourceids.ts
+++ src/ella2023/resourceids.ts
@@ -1,6 +1,21 @@
 export enum BitmapId {
 	None = 'None',
 
+	au_p1 = 'au_p1',
+	au_p2 = 'au_p2',
+	eila_duck = 'eila_duck',
+	eila_duckkick = 'eila_duckkick',
+	eila_flyingkick = 'eila_flyingkick',
+	eila_highkick = 'eila_highkick',
+	eila_humiliated = 'eila_humiliated',
+	eila_idle = 'eila_idle',
+	eila_jump = 'eila_jump',
+	eila_lowkick = 'eila_lowkick',
+	eila_punch = 'eila_punch',
+	eila_walk = 'eila_walk',
+	hallway = 'hallway',
+	hud = 'hud',
+	hud2 = 'hud2',
 	lee_duckorjump = 'lee_duckorjump',
 	lee_flyingkick = 'lee_flyingkick',
 	lee_highkick = 'lee_highkick',
@@ -10,7 +25,9 @@ export enum BitmapId {
 	lee_lowkick = 'lee_lowkick',
 	lee_punch = 'lee_punch',
 	lee_walk = 'lee_walk',
+	poef = 'poef',
 	whitepixel = 'whitepixel',
+	wuguan = 'wuguan',
 	_atlas = '_atlas',
 }
 
