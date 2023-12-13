7566d0f4dcbfb4283c4d0192f952b446b1498224
Boaz Pat-El <boazpat_el@hotmail.com>
2023-12-13 02:27:43 +0100

    Improve error handling & fetch APIness

--- scripts/rom.ts
+++ scripts/rom.ts
@@ -3,7 +3,7 @@ import { RomPack, RomAsset, RomMeta } from '../src/bmsx/rompack';
 /**
  * Pako is a high-speed zlib port to JavaScript, which is used to compress and decompress data.
  */
-declare var pako: any;
+declare const pako: any;
 /**
  * Function that initializes the boot ROM and starts the game.
  * @param {RomPack} rom - The boot ROM pack.
@@ -16,7 +16,7 @@ declare var h406A: (rom: RomPack, sndcontext: AudioContext, gainnode: GainNode, 
 /**
  * Object representing the boot ROM.
  */
-var bootrom = {
+const bootrom = {
 	/**
 	 * This section of code defines the boot ROM object and its properties and methods.
 	 *
@@ -65,23 +65,38 @@ var bootrom = {
 	 * @param x - The value to return after the game is started.
 	 * @returns 255 after the game is started.
 	 */
-	async usr(x: number): Promise<number> {
-		document.body.classList.add('game-started'); // Change background color of body
-		document.getElementById('gamescreen')!.hidden = false;
-		document.getElementById('gamescreen')!.style.display = 'block';
-		if (this.debug) {
-			document.getElementById('debugPanel')!.hidden = false;
-			document.getElementById('debugPanel')!.style.display = 'block';
-		}
-		// try {
-			await loadScript(bootrom.rom!, bootrom.romname);
+	usr(x: number): number {
+		const remove = (id: string) => {
+			let element = document.querySelector(id);
+			if (element) element.parentElement!.removeChild(element);
+		};
+
+		const wrapup = () => {
+			(document.querySelector('#loading') as HTMLElement).hidden = true;
+			remove('#msx');
+			remove('#hidor');
+			remove('#romjs');
+			document.body.classList.add('game-started'); // Change background color of body
+		};
+
+		// if (this.debug) {
+		// 	document.getElementById('debugPanel')!.hidden = false;
+		// 	document.getElementById('debugPanel')!.style.display = 'block';
+		// }
+		try {
+			if (!h406A) throw new Error('h406A is not defined!');
+			document.getElementById('gamescreen')!.hidden = false;
+			document.getElementById('gamescreen')!.style.display = 'block';
 			h406A(bootrom.rom!, bootrom.sndcontext!, bootrom.gainnode!, this.debug);
-			bootrom.rom = null;
+			wrapup();
+			bootrom.rom = undefined;
 			return x;
-		// } catch (err) {
-			// throw err ?? 'usr(x) failed with unknown error!';
-		// };
-		// return 255;
+		} catch (err) {
+			console.error(err);
+			document.getElementById('gamescreen')!.hidden = true;
+			document.getElementById('gamescreen')!.style.display = 'none';
+			throw new Error(`Error in usr(0): "${err?.message ?? err ?? 'unknown error :-('}"`);
+		}
 	},
 
 	/**
@@ -91,62 +106,73 @@ var bootrom = {
 	 */
 	async bload(url: string): Promise<RomPack | null> {
 		window.onunhandledrejection = (event: PromiseRejectionEvent) => {
-			console.log(`Unhandled rejection during "bload"-command: "${event.reason}".`);
 			event.preventDefault();
 			event.stopPropagation();
 			event.stopImmediatePropagation();
-			throw event.reason ?? '"bload(url)" failed with unknown error!';
-			// return false;
+			const reason = event.reason?.message ?? event.reason ?? 'unkown error';
+			const errormsg = `Unhandled rejection during "bload"-command: "${reason}".`;
+			throw new Error(errormsg);
 		};
 
 		createAudioContext();
 
-		let fetchRom = () => new Promise<ArrayBuffer>(async (resolve, reject) => {
-			if (bootrom.localfetch) {
-				fetchLocal(url).then(response_array => resolve(response_array)).catch(err => reject(err));
-			}
-			else {
-				fetch(url).then(response => response.arrayBuffer()).then(response_array => resolve(response_array)).catch(err => reject(err));
-			}
-		});
+		const fetchRom = () => {
+			return fetchBuffer(url).catch(err => {
+				throw new Error(`Error while fetching ROM: "${err.message}"`);
+			});
+		};
 
-
-		return new Promise(async (resolve, reject) => {
-			let bootCompletePromise = awaitBootComplete();
-			let result: RomPack | null = null;
+		return new Promise((resolve, reject) => {
+			let loadedRomPack: RomPack | null = null;
 			let romlabel_bloburl: string = undefined;
+
 			fetchRom()
 				.then((response_array: ArrayBuffer) => getZippedRomAndRomLabelFromBlob(response_array))
-				.then((ziprom_and_label: { zipped_rom: ArrayBuffer, romlabel: string }) => { romlabel_bloburl = ziprom_and_label.romlabel; return pako.inflate(ziprom_and_label.zipped_rom).buffer; })
+				.then((ziprom_and_label: { zipped_rom: ArrayBuffer, romlabel: string }) => {
+					romlabel_bloburl = ziprom_and_label.romlabel;
+					return pako.inflate(ziprom_and_label.zipped_rom).buffer;
+				})
 				.then(rom => loadResources(rom))
-				.then((loadResult: any) => result = loadResult)
-				.catch(err => reject(err));
-
-			await bootCompletePromise;
-			// if (romlabel_bloburl) {
-			// 	let msx = document.querySelector('#msx') as HTMLElement;
-			// 	debugger;
-			// 	delete msx.onanimationend;
-			// 	msx.className = 'fade-out';
-			// 	msx.onanimationend = (ev: AnimationEvent) => {
-			// 		let msx = ev.target as HTMLImageElement;
-			// 		msx.src = romlabel_bloburl;
-			// 		msx.className = 'fade-in';
-			// 	};
-			// }
-			setLoaderText('Press any key or touch screen to start...');
-			setClassForLoader('');
-			let pressedAnyKey = awaitPressedAnyKey();
-			await pressedAnyKey;
-
-			resolve(result);
+				.then((loadResult: any) => {
+					loadedRomPack = loadResult;
+					awaitBootComplete();
+				})
+				.then(() => loadScript(loadedRomPack, bootrom.romname))
+				.then(() => {
+					setLoaderText('Press any key or touch screen to start...');
+					setClassForLoader('');
+					return awaitPressedAnyKeyPromise();
+				})
+				.then(() => resolve(loadedRomPack))
+				.catch(err => {
+					reject(err);
+				});
 		});
+		// if (romlabel_bloburl) {
+		// 	let msx = document.querySelector('#msx') as HTMLElement;
+		// 	debugger;
+		// 	delete msx.onanimationend;
+		// 	msx.className = 'fade-out';
+		// 	msx.onanimationend = (ev: AnimationEvent) => {
+		// 		let msx = ev.target as HTMLImageElement;
+		// 		msx.src = romlabel_bloburl;
+		// 		msx.className = 'fade-in';
+		// 	};
+		// }
 	},
 
 	outputError(errormsg: string) {
+		console.error(errormsg);
+		bootrom.theshowsover = true;
+		const loadingElement = document.querySelector<HTMLElement>('#loading')
+		if (loadingElement) loadingElement.hidden = false;
+		const msxElement = document.querySelector<HTMLElement>('#msx');
+		if (msxElement) msxElement.onanimationend = undefined;
+		const hidorElement = document.querySelector<HTMLElement>('#hidor');
+		if (hidorElement) hidorElement.className = 'showsover';
+		document.body.className = "showsover";
 		setClassForLoader('');
 		setLoaderText(errormsg);
-		console.error(errormsg);
 	}
 };
 
@@ -211,7 +237,11 @@ async function getZippedRomAndRomLabelFromBlob(blob_buffer: ArrayBuffer): Promis
 	// }
 
 	// return Promise.resolve({ zipped_rom: getSubBufferAsPerMeta(blob_buffer, blob_meta), romlabel: romlabel_htmlimg });
-	return Promise.resolve({ zipped_rom: getSubBufferFromBufferWithMeta(blob_buffer), romlabel: undefined });
+	try {
+		return { zipped_rom: getSubBufferFromBufferWithMeta(blob_buffer), romlabel: undefined };
+	} catch (err) {
+		throw new Error(`Error in getZippedRomAndRomLabelFromBlob: "${err.message}"`);
+	}
 }
 
 /**
@@ -340,45 +370,30 @@ async function awaitBootComplete(): Promise<void> {
  * If an error occurs during loading, the Promise is rejected with an error message.
  */
 async function loadScript(rom: RomPack, romname: string): Promise<void> {
-	let result: Promise<void> = new Promise((resolve, reject) => {
+	try {
+		let scriptText: string;
+		if (!bootrom.debug) {
+			scriptText = rom.code;
+		} else {
+			const response = await fetchText(`../rom/${romname}.js`);
+			if (!response) throw new Error(`Failed to fetch file: ${romname}.js`);
+			scriptText = response;
+		}
+
 		let romcode = document.createElement('script');
 		romcode.async = false;
-		const rejectOnError = (event: ErrorEvent | string) => {
-			const errorMessage = typeof event === 'string' ? event : event.message;
-			reject(errorMessage);
-		};
-
-		romcode.onerror = rejectOnError;
-		window.onerror = rejectOnError;
-		if (!bootrom.debug) {
-			romcode.innerText = rom.code;
-			document.head.appendChild(romcode);
-			resolve();
-		} else {
-			romcode.src = `../${romname}.js`;
-			romcode.onload = () => resolve();
-			document.head.appendChild(romcode);
-		}
-	});
-	return result;
+		romcode.textContent = scriptText;
+		document.head.appendChild(romcode);
+	} catch (err) {
+		throw new Error(`Error in loadScript: ${err.message}`);
+	}
 }
 
 /**
  * Waits for the user to press any key before resolving the Promise.
  * @returns A Promise that resolves when the user presses any key.
  */
-async function awaitPressedAnyKey(): Promise<void> {
-	const remove = (id: string) => {
-		let element = document.querySelector(id);
-		if (element) element.parentElement!.removeChild(element);
-	};
-	const wrapup = () => {
-		(document.querySelector('#loading') as HTMLElement).hidden = true;
-		remove('#msx');
-		remove('#hidor');
-		remove('#romjs');
-	};
-
+async function awaitPressedAnyKeyPromise(): Promise<void> {
 	const result: Promise<void> = new Promise((resolve, reject) => {
 		const onuserinteraction = (e: UIEvent) => {
 			try {
@@ -396,7 +411,6 @@ async function awaitPressedAnyKey(): Promise<void> {
 				}
 				document.body.removeEventListener('keyup', onuserinteraction);
 				document.body.removeEventListener('touchend', onuserinteraction);
-				wrapup();
 				resolve();
 			}
 			catch (err) {
@@ -436,7 +450,32 @@ function setClassForLoader(cls: string) {
  * @returns {string}
  */
 function decodeuint8arr(to_decode: Uint8Array): string {
-	return new TextDecoder("utf-8").decode(to_decode);
+	const decoder = new TextDecoder("utf-8", { fatal: true });
+	try {
+		return decoder.decode(to_decode);
+	} catch (err) {
+		throw err;//new Error('Invalid UTF-8 input');
+	}
+}
+
+/**
+ * Fetches the text content from the specified URL.
+ * @param url The URL to fetch the text from.
+ * @returns A promise that resolves to the fetched text content.
+ * @throws If there is an error while fetching the text.
+ */
+async function fetchText(url: string): Promise<string> {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Failed to fetch file: ${url}`);
+		}
+		const decoder = new TextDecoder('utf-8');
+		const data = await response.arrayBuffer();
+		return decoder.decode(data);
+	} catch (err) {
+		throw new Error(`Error in fetchText: "${err.message}"`);
+	}
 }
 
 /**
@@ -445,20 +484,22 @@ function decodeuint8arr(to_decode: Uint8Array): string {
  * @returns A Promise that resolves with the fetched ArrayBuffer.
  * If an error occurs during fetching, the Promise is rejected with an error message.
  */
-async function fetchLocal(url: string): Promise<ArrayBuffer> {
-	return new Promise(function (resolve, reject) {
-		var xhr = new XMLHttpRequest;
-		xhr.responseType = "arraybuffer";
-		xhr.onload = function () {
-			resolve(xhr.response);
-		};
-		xhr.onabort = xhr.ontimeout = xhr.onerror = function (ev: ProgressEvent) {
-			// On error, [ev.target.statusText] is empty :-(
-			reject(`Failed to download rompack from "${url}" :-(`);
-		};
-		xhr.open('GET', url);
-		xhr.send(null);
-	});
+async function fetchBuffer(url: string): Promise<ArrayBuffer> {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Failed to fetch file: ${url}`);
+		}
+		return await response.arrayBuffer();
+	} catch (err) {
+		throw new Error(`Error in fetchBuffer: "${err.message}"`);
+	}
+}
+
+function removeEventListeners() {
+	// Remove event listener if it wasn't removed already
+	document.removeEventListener('keyup', startAudioOnIos, true);
+	document.removeEventListener('touchend', startAudioOnIos, true);
 }
 
 /**
@@ -467,13 +508,7 @@ async function fetchLocal(url: string): Promise<ArrayBuffer> {
  * @returns void
  */
 function startAudioOnIos(): void {
-	if (!bootrom.sndcontext) { return; }
-	const removeEventListeners = () => {
-		// Remove event listener if it wasn't removed already
-		document.removeEventListener('keyup', startAudioOnIos);
-		document.removeEventListener('touchend', startAudioOnIos, true);
-	};
-
+	if (!bootrom.sndcontext) return;
 	if (bootrom.snd_unlocked) {
 		// Remove event listener if it wasn't removed already
 		removeEventListeners();
