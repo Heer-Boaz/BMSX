7ac0828b794da6c26f3153e8ce4f672eeab94898
Boaz Pat-El <boazpat_el@hotmail.com>
2023-12-23 03:04:37 +0100

    Update new_area to new_area3d in Hud class

--- src/ella2023/hud.ts
+++ src/ella2023/hud.ts
@@ -1,7 +1,7 @@
 import { SpriteObject } from '../bmsx/sprite';
 import { BitmapId } from './resourceids';
 import { machine_states, statedef_builder } from '../bmsx/bfsm';
-import { get_gamemodel, new_area } from '../bmsx/bmsx';
+import { get_gamemodel, new_area3d } from '../bmsx/bmsx';
 import { GLView } from '../bmsx/glview';
 import { Msx1Colors } from '../bmsx/msx';
 import { gamemodel } from './gamemodel';
@@ -43,9 +43,9 @@ export class Hud extends SpriteObject {
         const hp1EndX = HP_BAR1.startX + (HP_BAR1.endX - HP_BAR1.startX) * hp1 / MAX_HP;
         const hp2EndX = HP_BAR2.endX - (HP_BAR2.endX - HP_BAR2.startX) * hp2 / MAX_HP;
 
-        let area = new_area(HP_BAR1.startX, HP_BAR1.startY, hp1EndX, HP_BAR1.endY, Z, Z)
+        let area = new_area3d(HP_BAR1.startX, HP_BAR1.startY, Z, hp1EndX, HP_BAR1.endY, Z);
         view.fillRectangle({ area, color });
-        area = new_area(hp2EndX, HP_BAR2.startY, HP_BAR2.endX, HP_BAR2.endY, Z, Z)
+        area = new_area3d(hp2EndX, HP_BAR2.startY, Z, HP_BAR2.endX, HP_BAR2.endY, Z);
         view.fillRectangle({ area, color });
 
         TextWriter.drawText(40, 32, 'sen kai la');
