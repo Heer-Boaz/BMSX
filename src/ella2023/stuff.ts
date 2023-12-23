9d534b64105b3ee62f649096963e2aaab38974ef
Boaz Pat-El <boazpat_el@hotmail.com>
2023-12-23 03:05:10 +0100

    Update imports and modify paint method

--- src/ella2023/stuff.ts
+++ src/ella2023/stuff.ts
@@ -1,10 +1,12 @@
 import { SpriteObject } from '../bmsx/sprite';
 import { BitmapId } from './resourceids';
 import { machine_states, statedef_builder } from '../bmsx/bfsm';
-import { get_gamemodel, new_vec3 } from '../bmsx/bmsx';
+import { get_gamemodel, new_area3d, new_vec3 } from '../bmsx/bmsx';
 import { gamemodel } from './gamemodel';
 import { TextWriter } from '../bmsx/textwriter';
 import { Input } from '../bmsx/input';
+import { DrawRectOptions } from '../bmsx/view';
+import { Msx1Colors } from '../bmsx/msx';
 
 const get_model = get_gamemodel<gamemodel>;
 
@@ -37,10 +39,12 @@ export class GameOver extends SpriteObject {
 
     override paint(): void {
         super.paint();
-        const x = 16;
+        const x = 8;
         const y = 144;
 
-        const lines = ['je bent toch niet', 'de strijder die ik nodig heb..', 'ik ben toch', 'lichtelijk teleurgesteld...'];
+        const options: DrawRectOptions = { area: new_area3d(0, 136, this.z + 1, 256, 192 - 8, this.z + 1), color: Msx1Colors[0] };
+        global.view.fillRectangle(options);
+        const lines = ['je bent toch niet', 'de strijder die ik nodig heb.', 'ik ben een beetje', 'teleurgesteld in jouw ouders...'];
 
         TextWriter.drawText(x, y, lines);
     }
@@ -59,16 +63,16 @@ export class Hoera extends SpriteObject {
                 _default: {
                     ticks2move: 500,
                     run(this: TitleScreen) {
-                        const priorityActions = Input.getPlayerInput(1).getPressedActions({ pressed: true, consumed: false, filter: ['punch', 'highkick', 'lowkick', 'block'] });
+                        // const priorityActions = Input.getPlayerInput(1).getPressedActions({ pressed: true, consumed: false, filter: ['punch', 'highkick', 'lowkick', 'block'] });
 
-                        // If no priority actions are pressed, do nothing.
-                        if (!priorityActions || priorityActions.length === 0) {
-                            return;
-                        }
-                        Input.getPlayerInput(1).consumeActions(...priorityActions);
+                        // // If no priority actions are pressed, do nothing.
+                        // if (!priorityActions || priorityActions.length === 0) {
+                        //     return;
+                        // }
+                        // Input.getPlayerInput(1).consumeActions(...priorityActions);
 
-                        // If a priority action is pressed, go to the game.
-                        get_model().sc.to('titlescreen');
+                        // // If a priority action is pressed, go to the game.
+                        // get_model().sc.to('titlescreen');
 
                     },
                     end(this: Hoera) {
@@ -82,9 +86,11 @@ export class Hoera extends SpriteObject {
     override paint(): void {
         super.paint();
         const x = 16;
-        const y = 144;
+        const y = 160;
 
-        const lines = ['dat heb je', 'redelijk gedaan Ella!', 'ik bedoel: ei la!'];
+        const options: DrawRectOptions = { area: new_area3d(0, 152, this.z + 1, 256, 192, this.z + 1), color: Msx1Colors[0] };
+        global.view.fillRectangle(options);
+        const lines = ['dat heb je', 'redelijk gedaan Elly!', 'ik bedoel: Ei La!'];
 
         TextWriter.drawText(x, y, lines);
     }
