f20afb69bb6972941a57203fe76c02782cd2ad5d
Boaz Pat-El <boazpat_el@hotmail.com>
2024-12-08 15:24:11 +0100

    Refactor sint class to improve image handling and state transitions

--- src/sint2024/sint.ts
+++ src/sint2024/sint.ts
@@ -5,43 +5,63 @@ import { SpriteObject, StateMachineBlueprint, build_fsm, insavegame } from '../b
 export class sint extends SpriteObject {
     constructor() {
         super('sint');
-        this.imgid = 'hmm'; // Set the image to be displayed to 'hmm' so that the size properties are calculated
-        // Place sint at lower-right corner of the screen by using the
-        // screen width and height minus the sprite width and height
-        const gamescreenSize = $.getViewportSize();
-        this.x = gamescreenSize.x - this.sx;
-        this.y = gamescreenSize.y - this.sy;
+        this.setimg('hmm');
+    }
+
+    private setimg(imgid: string) {
+        this.imgid = imgid; // Set the image to be displayed to 'hmm' so that the size properties are calculated
+        if (this.imgid !== 'none') {
+            const gamescreenSize = $.getViewportSize();
+            this.x = gamescreenSize.x - this.sx;
+            this.y = gamescreenSize.y - this.sy;
+        }
     }
 
     @build_fsm()
     static bouw(): StateMachineBlueprint {
         return {
             states: {
-                _vraag: {
+                _weg: {
                     on: {
+                        vraag: 'vraag',
                         antwoord: 'antwoord',
                         klaar: 'klaar',
                     },
                     enter(this: sint) {
-                        this.imgid = 'hmm';
+                        this.visible = false;
+                    },
+                    exit(this: sint) {
+                        this.visible = true;
+                    },
+                },
+                vraag: {
+                    on: {
+                        antwoord: 'antwoord',
+                        klaar: 'klaar',
+                        weg: 'weg',
+                    },
+                    enter(this: sint) {
+                        this.setimg('hmm');
                     },
                 },
                 antwoord: {
                     on: {
                         vraag: 'vraag',
                         klaar: 'klaar',
+                        weg: 'weg',
                     },
                     enter(this: sint) {
-                        this.imgid = 'goed';
+                        this.setimg('goed');
                     },
                 },
                 klaar: {
                     on: {
                         antwoord: 'antwoord',
                         vraag: 'vraag',
+                        weg: 'weg',
                     },
                     enter(this: sint) {
-                        this.imgid = 'klaar';
+                        this.setimg('klaar');
                     },
                 },
             }
