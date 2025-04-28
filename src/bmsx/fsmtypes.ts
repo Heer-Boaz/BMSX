34a24344fde3b730f72a857d95a04cbd6bf1808d
Boaz Pat-El <boaz@ch.tudelft.nl>
2025-04-28 15:14:35 +0200

    fix: remove trailing dots from state prefix constants for bugfixing

--- src/bmsx/fsmtypes.ts
+++ src/bmsx/fsmtypes.ts
@@ -1,8 +1,8 @@
 import type { EventScope, Identifier, IEventSubscriber, IRegisterable, State, StateDefinition, StateMachineController } from "./bmsx";
 
-export const STATE_THIS_PREFIX = '#this.';
-export const STATE_PARENT_PREFIX = '#parent.';
-export const STATE_ROOT_PREFIX = '#root.';
+export const STATE_THIS_PREFIX = '#this';
+export const STATE_PARENT_PREFIX = '#parent';
+export const STATE_ROOT_PREFIX = '#root';
 
 /**
  * Represents a type definition for mapping IDs to `sdef` objects.
