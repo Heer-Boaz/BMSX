0136afb5474e9da9b9760d15dc1ae370894d536c
Boaz Pat-El <boaz@ch.tudelft.nl>
2025-07-26 03:24:04 +0200

    feat: Move bvec utility functions to vertexutils2d.ts for better organization

--- src/bmsx/render/glview.constants.ts
+++ src/bmsx/render/glview.constants.ts
@@ -37,27 +37,3 @@ export const COLOR_OVERRIDE_COMPONENTS = 4;
 export const ATLAS_ID_COMPONENTS = 1;
 
 export const SPRITE_DRAW_OFFSET = 0;
-
-export const bvec = {
-    set(v: Float32Array, i: number, x: number, y: number, w: number, h: number, sx: number, sy: number): void {
-        const x2 = x + w * sx, y2 = y + h * sy, offset = i * VERTEXCOORDS_SIZE;
-        v.set([x, y, x2, y, x, y2, x, y2, x2, y, x2, y2], offset);
-    },
-    set_texturecoords(v: Float32Array, i: number, coords: number[]): void {
-        const offset = i * TEXTURECOORDS_SIZE;
-        v.set(coords, offset);
-    },
-    set_zcoord(v: Float32Array, i: number, z: number): void {
-        const offset = i * ZCOORDS_SIZE;
-        for (let j = offset; j < offset + ZCOORDS_SIZE; j += ZCOORD_ATTRIBUTE_SIZE) v[j] = z;
-    },
-    set_color(v: Float32Array, i: number, color: Color): void {
-        const offset = i * COLOR_OVERRIDE_SIZE;
-        const colorArray = [color.r, color.g, color.b, color.a];
-        for (let j = offset; j < offset + COLOR_OVERRIDE_SIZE; j += COLOR_OVERRIDE_ATTRIBUTE_SIZE) v.set(colorArray, j);
-    },
-    set_atlas_id(v: Uint8Array, i: number, atlas_id: number): void {
-        const offset = i * ATLAS_ID_SIZE;
-        for (let j = offset; j < offset + ATLAS_ID_SIZE; j += ATLAS_ID_ATTRIBUTE_SIZE) v[j] = atlas_id;
-    }
-};
