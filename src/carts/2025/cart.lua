68d192203aa1eb24cb74e7eddafb5900b892b856
Boaz Pat-El <boazpat_el+github@hotmail.com>
2025-12-13 02:37:42 +0100

    Sinterklaasgame

--- src/carts/2025/cart.lua
+++ src/carts/2025/cart.lua
@@ -1,6 +1,7 @@
 local states = {
 	title = 'title',
 	game = 'game',
+	overgang = 'overgang',
 }
 
 local gamestate = states.title
@@ -9,19 +10,32 @@ local function draw_title()
 	sprite('titel', 0, 0, 0)
 end
 
+local overgang_x = 0
+local overgang_y = 64
+local function init_overgang()
+	overgang_x = display_width
+end
+
+local function draw_overgang()
+	write('MONDAY', overgang_x, overgang_y, 0, 15)
+end
+
+local function update_overgang()
+	overgang_x -= 4
+	if overgang_x < -48 then
+		emit_gameplay('overgang_klaar', 'world')
+	end
+end
+
 local function draw_game()
 	cls(4)
 end
 
-local draw_handlers = {
-	title=draw_title,
-	game=draw_game,
-}
-
 local function update_title()
 	if action_triggered('a[jp]') then
 		$.stopmusic()
-		gamestate = states.game
+		$.playaudio('m05')
+		gamestate = states.overgang
 	end
 end
 
@@ -31,6 +45,13 @@ end
 local update_handlers = {
 	title=update_title,
 	game=update_game,
+	overgang=update_overgang,
+}
+
+local draw_handlers = {
+	title=draw_title,
+	game=draw_game,
+	overgang=draw_overgang,
 }
 
 function init()
@@ -42,7 +63,7 @@ function new_game()
 end
 
 function update()
-	print(gamestate)
+	-- print(gamestate)
 	update_handlers[gamestate]()
 end
 
