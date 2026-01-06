e5ae8017904adf0b40298894c879a75ee082cf86
Boaz Pat-El <boazpat_el+github@hotmail.com>
2026-01-06 15:17:37 +0100

    feat: Update AAC encoding bitrate to 16 for improved audio quality

--- scripts/aac-test.ts
+++ scripts/aac-test.ts
@@ -4,6 +4,6 @@ import { encodeWavToAacLc } from './rompacker/audioencoder'
 (async () => {
 	const path = '../src/carts/2025/res/m02@m@l=0.wav';
 	const wav = await fs.readFile(path);
-	const aac = await encodeWavToAacLc(wav, path, { bitrate: 128 });
+	const aac = await encodeWavToAacLc(wav, path, { bitrate: 16 });
 	await fs.writeFile("../output.aac", aac);
 })();
