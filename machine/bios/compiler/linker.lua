a89a9647916b4982ac04e39e1a71eec36c018ef4
Boaz Pat-El <boazpat_el+github@hotmail.com>
2026-08-09 05:01:05 +0200

    perf(bios): link runtime records without address tables

--- machine/bios/compiler/linker.lua
+++ machine/bios/compiler/linker.lua
@@ -6,34 +6,34 @@ local linker<const> = {}
 
 function linker.link(program)
 	local protos<const> = program.protos
-	local record_bytes<const> = #protos * isa.function_record_size
-	local upvalue_bytes = 0
-	local code_bytes = 0
+	local function_record_byte_count<const> = #protos * isa.function_record_size
+	local upvalue_byte_count = 0
+	local code_byte_count = 0
 	for index = 1, #protos do
 		local proto<const> = protos[index]
-		upvalue_bytes = upvalue_bytes
+		upvalue_byte_count = upvalue_byte_count
 			+ #proto.upvalue_registers * isa.upvalue_record_size
-		code_bytes = code_bytes + #proto.words * isa.instruction_bytes
+		code_byte_count = code_byte_count
+			+ #proto.words * isa.instruction_bytes
 	end
-	local code_offset<const> = (record_bytes + upvalue_bytes + 3) & -4
-	local block<const> = arena.allocate(
-		code_offset + code_bytes,
+	local code_byte_offset<const> = (
+		function_record_byte_count + upvalue_byte_count + 3
+	) & -4
+	local function_table_address<const> = arena.allocate(
+		code_byte_offset + code_byte_count,
 		isa.function_alignment
 	)
 
-	local record_addresses<const> = {}
-	for index = 1, #protos do
-		record_addresses[index] = block + (index - 1) * isa.function_record_size
-	end
 	local const_pool<const> = program.const_pool
 	local const_relocations<const> = program.const_relocations
 	for index = 1, #const_relocations do
 		local relocation<const> = const_relocations[index]
-		const_pool[relocation.const_index] = record_addresses[relocation.proto_index]
+		const_pool[relocation.const_index] = function_table_address
+			+ (relocation.proto_index - 1) * isa.function_record_size
 	end
 
-	local upvalue_address = block + record_bytes
-	local code_address = block + code_offset
+	local upvalue_address = function_table_address + function_record_byte_count
+	local code_address = function_table_address + code_byte_offset
 	for index = 1, #protos do
 		local proto<const> = protos[index]
 		local upvalue_registers<const> = proto.upvalue_registers
@@ -45,21 +45,23 @@ function linker.link(program)
 			)
 			upvalue_address = upvalue_address + isa.upvalue_record_size
 		end
-		local proto_code_bytes<const> = #proto.words * isa.instruction_bytes
+		local proto_code_byte_count<const> = #proto.words * isa.instruction_bytes
 		bytecode.write_function_record(
-			record_addresses[index],
+			function_table_address + (index - 1) * isa.function_record_size,
 			code_address,
-			proto_code_bytes,
+			proto_code_byte_count,
 			proto.parameter_count,
 			proto.max_stack,
 			proto_upvalue_address,
 			#upvalue_registers
 		)
 		bytecode.write(proto.words, code_address)
-		code_address = code_address + proto_code_bytes
+		code_address = code_address + proto_code_byte_count
 	end
 
-	return record_addresses[program.root_proto_index], const_pool
+	local root_function_address<const> = function_table_address
+		+ (program.root_proto_index - 1) * isa.function_record_size
+	return root_function_address, const_pool
 end
 
 return linker
