754eb9aa90c1213543bf63efc5e0ac887461ecb3
Boaz Pat-El <boazpat_el+github@hotmail.com>
2026-08-09 05:59:55 +0200

    fix(bios): align runtime allocations by address

--- machine/bios/compiler/arena.lua
+++ machine/bios/compiler/arena.lua
@@ -6,13 +6,19 @@ bss runtime_code_arena: u8[arena_capacity]
 bss runtime_code_arena_offset: word
 
 function arena.allocate(byte_count, alignment)
-	local offset<const> = (*runtime_code_arena_offset + alignment - 1) & -alignment
-	local next_offset<const> = offset + byte_count
+	local arena_address<const> = &runtime_code_arena
+	local address<const> = (
+		arena_address
+		+ *runtime_code_arena_offset
+		+ alignment
+		- 1
+	) & -alignment
+	local next_offset<const> = address + byte_count - arena_address
 	if next_offset > arena_capacity then
 		error('runtime compiler memory exhausted')
 	end
 	*runtime_code_arena_offset = next_offset
-	return &runtime_code_arena + offset
+	return address
 end
 
 return arena
