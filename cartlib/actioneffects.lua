39c280d9b215e48274d8a8571a1b6fbad70c0415
Boaz Pat-El <boazpat_el+github@hotmail.com>
2026-08-10 19:16:43 +0200

    refactor(cartlib): keep action effects cart-defined

--- cartlib/actioneffects.lua
+++ cartlib/actioneffects.lua
@@ -3,17 +3,6 @@ local registry<const> = require('cartlib/registry')
 
 local actioneffects<const> = {}
 
-actioneffects.effect_type = {
-	spawn = 'spawn',
-	despawn = 'despawn',
-	damage = 'damage',
-	heal = 'heal',
-	move = 'move',
-	play_sound = 'play_sound',
-	play_animation = 'play_animation',
-	emit_event = 'emit_event',
-}
-
 function actioneffects.register_effect(id, definition)
 	actioneffectcomponent.set_definition(id, definition)
 	local components<const> = registry:entries(actioneffectcomponent)
@@ -24,18 +13,5 @@ function actioneffects.register_effect(id, definition)
 		end
 	end
 end
-
-actioneffects.register_effect(actioneffects.effect_type.move, {
-	handler = function(owner, _payload, dx, dy)
-		owner.x = owner.x + dx
-		owner.y = owner.y + dy
-	end,
-})
-
-actioneffects.register_effect(actioneffects.effect_type.play_animation, {
-	handler = function(owner, _payload, animation_id, options)
-		owner.timelines:play(animation_id, options)
-	end,
-})
 
 return actioneffects
