3df0642717050c867e592c5cd9211f287cbd482c
Boaz Pat-El <boazpat_el+github@hotmail.com>
2026-08-12 04:27:09 +0200

    perf(nemesis-s): omit unused input action system

--- carts/nemesis_s/world_module.lua
+++ carts/nemesis_s/world_module.lua
@@ -1,6 +1,5 @@
 local actioneffectsystem<const> = require('cartlib/actioneffects/actioneffectsystem')
 local fsmsystem<const> = require('cartlib/fsm/fsmsystem')
-local inputactioneffectsystem<const> = require('cartlib/input/actioneffect/system')
 local inputsystem<const> = require('cartlib/input/inputsystem')
 local timeline_system<const> = require('cartlib/timeline/timeline_system')
 
@@ -8,7 +7,6 @@ return {
 	spaces = { 'main' },
 	systems = {
 		inputsystem,
-		inputactioneffectsystem,
 		actioneffectsystem,
 		fsmsystem,
 		timeline_system,
