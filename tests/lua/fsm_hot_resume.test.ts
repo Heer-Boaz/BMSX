f909b83b11881b3508e1fdf7a501bebb6e6898ef
Boaz Pat-El <boazpat_el+github@hotmail.com>
2026-08-13 14:40:57 +0200

    refactor(cartlib): remove textual syntax pipeline

--- tests/lua/fsm_hot_resume.test.ts
+++ tests/lua/fsm_hot_resume.test.ts
@@ -30,8 +30,6 @@ const SYSTEM_MODULE_FILES = [
 ] as const;
 
 const CART_MODULE_FILES = [
-	['cartlib/codegen/lua_syntax', 'cartlib/codegen/lua_syntax.lua'],
-	['cartlib/codegen/lua_syntax_printer', 'cartlib/codegen/lua_syntax_printer.lua'],
 	['cartlib/util/dense_set', 'cartlib/util/dense_set.lua'],
 	['cartlib/component/component_class', 'cartlib/component/component_class.lua'],
 	['cartlib/registry', 'cartlib/registry.lua'],
